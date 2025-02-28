f15bd0fed0dd20a8c0d6148f0e3deefd8e26ee53
sdipe <cmrsman007@gmail.com>
2025-02-27 23:11:50 -0300

    feat: aprimorada exibição de informações do Ninja para o usuário implementado princípios de orientação a objetos

--- Actions.java
+++ Actions.java
@@ -1,24 +1,39 @@
 package Dados;
 
+
 import java.util.Scanner;
 
+public class Actions {
 
-public class Actions{
-    String[] ninjasName = new String[1];
-    Scanner scan = new Scanner(System.in);
-
-
-    public String[] Registration(String input) {
-        int numNinja = Integer.parseInt(input); //numero de ninjas a serem cadastrados
-        ninjasName = new String[numNinja];
-
-        for (int i=0;i<ninjasName.length;i++) {
-            System.out.printf("Registre o %d° Ninja da Vila da folha\n", i + 1);
-            System.out.print("Digite o nome: ");
-            ninjasName[i] = scan.nextLine();
-            }
-            return ninjasName;
-
-         }
-
+    public static void  NinjaRegistration(String input) {
+        Scanner scan = new Scanner(System.in);
+        int numNinja = Integer.parseInt(input);
+        for (int i = 0; i < numNinja; i++) {
+            System.out.println("Digite o nome do " + (i + 1) + "° ninja");
+            String name = scan.nextLine();
+            System.out.println("Digite a idade:");
+            int age = scan.nextInt();
+            scan.nextLine();
+            System.out.println("Digite a Missão atribuida:");
+            String assignedMission = scan.nextLine();
+            System.out.println("Digite a dificuldade da missão (A, B, C ou D:");
+            char difficulty = scan.nextLine().charAt(0);
+            Ninja novoninja = new Ninja(name, age, assignedMission, difficulty);
+            Main.ninjasData.add(novoninja);
+        }
     }
+
+        public void ShowRegistered() {
+        System.out.println("Ninjas cadastrados:");
+        for (int i = 0; i < Main.ninjasData.size() ; i++) {
+            Ninja showNinja = Main.ninjasData.get(i);
+            System.out.println((i + 1) + "° Ninja: " + showNinja.name);
+            System.out.println("Idade: " + showNinja.age);
+            System.out.println("Missão Atribuida: " + showNinja.assignedMission);
+            System.out.println("Dificuldade da Missão: " + Character.toUpperCase(showNinja.difficulty));
+        }
+
+        }
+    }
+
+
